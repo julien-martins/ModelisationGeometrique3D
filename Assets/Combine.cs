9f73b68e2c26ba072c6f12795b7827ad04f934b6
julien-martins <julien.martins-almoster@hotmail.com>
2022-10-10 11:58:29 +0200

    Try to combine multiple Sphere

---
+++ Assets/Combine.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Operation{
+        Union,
+        Intersection,
+        Melange,
+        Soustraction
+    }
+
+public class Combine : MonoBehaviour
+{
+    public Operation operation;    
+
+    //public List<VoxelSphere> Spheres;
+
+    [Header("Soustraction")]
+    public VoxelSphere Sphere1;
+    public VoxelSphere Sphere2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(operation){
+            case Operation.Union:
+                Union();
+                break;
+            case Operation.Intersection:
+                Intersection();
+                break;
+            case Operation.Melange:
+                Melange();
+                break;
+            case Operation.Soustraction:
+                Soustraction(Sphere1, Sphere2);
+                break;
+        }
+    }
+
+    void Union(){
+
+    }
+
+    void Intersection(){
+
+    }
+
+    void Melange(){
+
+    }
+
+    void Soustraction(VoxelSphere s1, VoxelSphere s2){
+        List<Cube> newCubes = new();
+
+        foreach(Cube c1 in s1.GetCubes()){
+            foreach(Cube c2 in s2.GetCubes()){
+                if(c1.pos != c2.pos) newCubes.Add(c1);
+            }
+        }
+
+        gameObject.AddComponent<VoxelSphere>();
+        gameObject.GetComponent<VoxelSphere>().UpdateMesh(newCubes);
+
+        s1.UpdateMesh(newCubes);
+    }
+}
