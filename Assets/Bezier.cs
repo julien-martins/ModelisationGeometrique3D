5e0076ddac1ef9238b0b4563f354b68f861bc567
julien-martins <39356114+julien-martins@users.noreply.github.com>
2022-10-24 15:39:16 +0200

    Bezier Curve

--- Assets/Bezier.cs
+++ Assets/Bezier.cs
@@ -6,18 +6,17 @@ public class Bezier : MonoBehaviour
 {
     public List<Transform> points;
 
-    void polyBernstein(){
+    Bezier Parent;
+    Bezier Child;
 
+    float Bernstein(int i, int n, float u)
+    {
+        return (Fact(n) / (Fact(i) * (Fact(n - i)))) * Mathf.Pow(u, i) * Mathf.Pow(1 - u, n - i);
     }
 
-    float bernstein(int i, int n, float t)
+    int Fact(int n)
     {
-        return (Factoriel(n) / (Factoriel(i) * (Factoriel(n - i)))) * Mathf.Pow(t, i) * Mathf.Pow(1 - t, n - i);
-    }
-
-    int Factoriel(int n)
-    {
-        return n > 1 ? n * Factoriel(n - 1) : 1;
+        return n > 1 ? n * Fact(n - 1) : 1;
     }
 
     // Start is called before the first frame update
@@ -33,13 +32,31 @@ public class Bezier : MonoBehaviour
             Gizmos.DrawLine(points_pos[i], points_pos[i+1]);
         }
 
+        //Calculate new points
         Gizmos.color = Color.red;
+        List<Vector3> newPoints = BezierCurve(points_pos);
+
+        for(int i = 0; i < newPoints.Count - 1; ++i){
+            Debug.Log(newPoints[i]);
+            Debug.Log(newPoints[i+1]);
+
+            Gizmos.DrawLine(newPoints[i], newPoints[i + 1]);
+        }
 
     }
+    
+    List<Vector3> BezierCurve(List<Vector3> points){
+        List<Vector3> result = new();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for(float i = 0.0f; i <= 1.0f; i += 0.01f){
+            Vector3 newPoint = Vector3.zero;
+            for(int j = 0; j < points.Count; j += 1){
+                newPoint += Bernstein(j, points.Count - 1, i) * points[j];                
+            }
+            result.Add(newPoint);
+        }
+        Debug.Log(result.Count);
+
+        return result;
     }
 }
